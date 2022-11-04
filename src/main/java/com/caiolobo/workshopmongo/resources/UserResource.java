33488e6e7d1b1eea01331bc0162eeb9607cb8a91
Caio Lobo <caioeduardolobo@gmail.com>
2022-11-04 10:21:05 -0300

    Using DTO to return users

--- src/main/java/com/caiolobo/workshopmongo/resources/UserResource.java
+++ src/main/java/com/caiolobo/workshopmongo/resources/UserResource.java
@@ -1,16 +1,15 @@
 package com.caiolobo.workshopmongo.resources;
 
 import com.caiolobo.workshopmongo.domain.User;
+import com.caiolobo.workshopmongo.dto.UserDTO;
 import com.caiolobo.workshopmongo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/users")
@@ -20,9 +19,10 @@ public class UserResource {
     private UserService service;
 
     @RequestMapping(method = RequestMethod.GET) // ou  @GetMapping
-    public ResponseEntity<List<User>> findAll(){      //ResponseEntity encapsula toda a estrutura necessária para retornar respsotas http com cabeçalhos, erros...
+    public ResponseEntity<List<UserDTO>> findAll(){      //ResponseEntity encapsula toda a estrutura necessária para retornar respsotas http com cabeçalhos, erros...
         List<User> list = service.findAll();
-        return ResponseEntity.ok().body(list);
+        List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).toList();
+        return ResponseEntity.ok().body(listDto);
     }
 
 }
