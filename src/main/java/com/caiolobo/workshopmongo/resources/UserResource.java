6adb683c1e164c28f3f002de1746e6a58d92fa05
Caio Lobo <caioeduardolobo@gmail.com>
2022-11-04 11:33:44 -0300

    Getting user by id

--- src/main/java/com/caiolobo/workshopmongo/resources/UserResource.java
+++ src/main/java/com/caiolobo/workshopmongo/resources/UserResource.java
@@ -5,9 +5,8 @@ import com.caiolobo.workshopmongo.dto.UserDTO;
 import com.caiolobo.workshopmongo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -23,6 +22,14 @@ public class UserResource {
         List<User> list = service.findAll();
         List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).toList();
         return ResponseEntity.ok().body(listDto);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<UserDTO> findById(@PathVariable String id){
+        User user = service.findById(id);
+
+        return ResponseEntity.ok().body(new UserDTO(user));
+
     }
 
 }
--- src/main/java/com/caiolobo/workshopmongo/services/UserService.java
+++ src/main/java/com/caiolobo/workshopmongo/services/UserService.java
@@ -2,10 +2,12 @@ package com.caiolobo.workshopmongo.services;
 
 import com.caiolobo.workshopmongo.domain.User;
 import com.caiolobo.workshopmongo.repository.UserRepository;
+import com.caiolobo.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -14,6 +16,11 @@ public class UserService {
 
     public List<User> findAll(){
         return repository.findAll();
+    }
+
+    public User findById(String id){
+        Optional<User> user = repository.findById(id);     // se não achar retorna nulo;
+        return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
     }
 
 }
